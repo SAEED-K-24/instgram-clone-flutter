c7875180f182df9f16ac9245e9ab1fb40b3e5430
saeed-k-24 <saeedkhaled88@gmail.com>
2022-06-11 19:39:09 +0300

    flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_task/controller/post_controller.dart';
 import 'package:flutter_application_task/controller/user_controller.dart';
 import 'package:flutter_application_task/provider/bottom_nav_bar_provider.dart';
+import 'package:flutter_application_task/view/screen/home_screen.dart';
 import 'package:flutter_application_task/view/screen/post_screen.dart';
 import 'package:flutter_application_task/view/screen/post_scroll_screen.dart';
 import 'package:flutter_application_task/view/screen/search_screen.dart';
@@ -32,24 +33,24 @@ class MyApp extends StatelessWidget {
       designSize: Size(340, 640),
       builder: () => MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ContentScreen(),
-      // Consumer<BottomNavBarProvider>(
-      //   builder: (context,p,ch)=>Scaffold(
-      //     backgroundColor: Colors.white,
-      //     body: p.widgets[p.index],
-      //     bottomNavigationBar: BottomNavigationBar(
-      //     backgroundColor: Colors.white,
-      //     elevation: 10,
-      //     currentIndex: p.index,
-      //     onTap: (i) {
-      //     p.changeIndex(i);
-      //     },
-      //   items: p.items,
-      //   ),
+      home: 
+      Consumer<BottomNavBarProvider>(
+        builder: (context,p,ch)=>Scaffold(
+          backgroundColor: Colors.white,
+          body: p.widgets[p.index],
+          bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          elevation: 10,
+          currentIndex: p.index,
+          onTap: (i) {
+          p.changeIndex(i);
+          },
+        items: p.items,
+        ),
         
-      //   ),
+        ),
         
-      // ),
+      ),
       routes: {
         PostsScrollScreen.postsScrollScreenRoute:(context)=>PostsScrollScreen(),
         SearchScreen.searchScreenRoute:(context)=>SearchScreen(),
--- lib/view/screen/profile_screen.dart
+++ lib/view/screen/profile_screen.dart
@@ -5,8 +5,102 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('profile screen'),
+     return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20,right: 14,left: 14,bottom: 2),
+        child: ListView(
+          children: 
+          [
+            Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: 
+            [
+              Text('Name name',style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+              IconButton(onPressed: (){}, icon: Icon(Icons.category))
+            ],
+            ),
+            SizedBox(height: 30,),
+            Container(
+              padding: EdgeInsets.only(right: 8),
+              width: MediaQuery.of(context).size.width,
+              height: 90,
+              child: Row(
+                mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                children: 
+                [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/user1.png'),
+                    radius: 45,
+                  ),
+                  
+                  Container(
+                    height: 50,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: 
+                      [
+                        Text('0'),
+                        Text('Posts'),
+                      ],
+                    ),
+                    
+                  ),
+                  
+                  Container(
+                    height: 50,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: 
+                      [
+                        Text('66'),
+                        Text('Followers'),
+                      ],
+                    ),
+                    
+                  ),
+                  
+                  Container(
+                    height: 50,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: 
+                      [
+                        Text('98'),
+                        Text('Following'),
+                      ],
+                    ),
+                    
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              margin: EdgeInsets.only(left: 2),
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: 
+                [
+                  Text('Name name'),
+                  SizedBox(height: 2,),
+                  Text('off'),
+                ],
+              ),
+            ),
+            SizedBox(height: 30,),
+            Container(
+              color: Colors.grey.shade300,
+              width: MediaQuery.of(context).size.width,
+              child: TextButton(onPressed: (){},child:Text('Edit Profile',style: TextStyle(color: Colors.black),)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
